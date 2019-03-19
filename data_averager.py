b9d166a8390199ba50c0b51f451414f836d37360
Alex <alexander.c.pesch@gmail.com>
2019-03-19 22:25:14 +0000

    Add new file to test usgs geomagnetism data file.

---
+++ data_averager.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+
+from sys import argv
+import csv
+from os import path
+
+error_message = ('Please provide full filepath to USGS Geomagnetism file as an 
+              'argument to be parsed'. Example: "python data_averager.py 
+              '/path/to/file"')
+
+
+def dataGrabber():
+    if len(argv) != 2:
+        print(error_message)
+        print 
+        
+    else:
+        file_path = argv[1]
+        if path.isfile(file_path):
+            with open(file_path, 'r') as geo_data:
+                geo_reader = csv.reader(geo_data, delimiter=' ')
+                for row in geo_reader:
+                    geo_dataset.append(row)
+                return geo_dataset
+        else:
+            print(error_message)
+            
+
+def dataParser(usgs_geo_data):
+    print usgs_geo_data
+    
+
+def main():
+    usgs_data_list = dataGrabber()
+    if usgs_data_list:
+        dataParser(usgs_data_list)
+    
+    
+if __name__ == '__main__':
+    main()
