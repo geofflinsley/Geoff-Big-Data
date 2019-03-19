f3b1f7f2350458a488170eedd3bdc884ed82d0be
Alex <alexander.c.pesch@gmail.com>
2019-03-19 22:35:58 +0000

    Update data_averager.py

--- data_averager.py
+++ data_averager.py
@@ -3,6 +3,7 @@
 from sys import argv
 import csv
 from os import path
+import re
 
 error_message = ('Please provide full filepath to USGS Geomagnetism file as an '
                  'argument to be parsed.\n Example: "python data_averager.py '
@@ -10,19 +11,33 @@ error_message = ('Please provide full filepath to USGS Geomagnetism file as an '
 
 
 def dataGrabber():
+    """ This function ensures the file exists, and it is valid.
+    
+    A valid file is a USGS Gemoganetism .txt file downloaded from the USGS
+    Geomagnetism website. It verifies it is a valid file by looking at the
+    header row. If a valid file isn't provided, this script will not continue.
+    
+        Returns: geo_dataset - A list of lists with each inner list containing
+            data from each row of the file.
+    """
+    
     geo_dataset = []
     
+    # If an argument is not provided with the script, then print help text.
     if len(argv) != 2:
         print(error_message)
-        print 
         
+    # If an argument is provided, check that it is a valid, existing file.
+    # If it is valid, clean up the extra white spaces.
+    
     else:
         file_path = argv[1]
         if path.isfile(file_path):
             with open(file_path, 'r') as geo_data:
                 geo_reader = csv.reader(geo_data, delimiter=' ')
                 for row in geo_reader:
-                    geo_dataset.append(row)
+                    clean_row = re.sub(' +', ' ',row)
+                    geo_dataset.append(clean_row)
                 return geo_dataset
         else:
             print(error_message)
