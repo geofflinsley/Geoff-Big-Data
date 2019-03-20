a03ac36e2bca11de5c7cb982b79c9e5b1dafc236
Alex <alexander.c.pesch@gmail.com>
2019-03-20 19:44:57 +0000

    Adding unit test file

---
+++ data_averager_test.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+import pytest
+import data_averager
+
+def test_dataGrabberFail():
+
+    fail_tests = [10, {'test': 1,2,3}, ['test'], '/some/path/to/somewhere']
+    
+    with pytest.raises(Exception):
+        for test in fail_tests:
+            data_averager.dataGrabber(test)
