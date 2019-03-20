c47f71d62031ec3c42403d5259dfb811ff763049
Alex <alexander.c.pesch@gmail.com>
2019-03-20 00:58:10 +0000

    Update data_averager.py

--- data_averager.py
+++ data_averager.py
@@ -49,11 +49,13 @@ def dataGrabber():
                     geo_dataset.append([clean_row])
         else:
             print(error_message)
-                
-    if geo_dataset[0] == ['Year Mon Day Hr DOY from start Fractional DOY '
-                          'Fractional year Dst HER KAK HON SJG sigma']:
-        print('Valid dataset detected. Continuing...')
-        return geo_dataset
+    if geo_dataset:
+        if geo_dataset[0] == ['Year Mon Day Hr DOY from start Fractional DOY '
+                              'Fractional year Dst HER KAK HON SJG sigma']:
+            print('Valid dataset detected. Continuing...')
+            return geo_dataset
+        else:
+            print(error_message)
     else:
         print(error_message)
             
