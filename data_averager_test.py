b2600b24b00ae958566e9140bd16d05afa833027
Alex <alexander.c.pesch@gmail.com>
2019-03-20 21:03:47 +0000

    Update data_averager_test.py

--- data_averager_test.py
+++ data_averager_test.py
@@ -2,13 +2,15 @@
 
 import pytest
 import data_averager
+from mock import patch
 
 def test_dataGrabberFail():
 
     fail_tests = [10, {'test': 1}, ['test'], '/some/path/to/somewhere']
     
-    for test in fail_tests:
-        assert data_averager.dataGrabber(test) == (
+    with patch.object(sys, 'argv', fail_tests):
+        averager_result = data_averager.dataGrabber()
+        assert averager_result == (
             'Please provide full filepath to USGS Geomagnetism file as an '
-            'argument to be parsed.\n Example: "python data_averager.py ' 
+            'argument to be parsed.\n Example: "python data_averager.py '
             '/path/to/file"')
