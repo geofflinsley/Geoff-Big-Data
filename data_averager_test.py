3b35c3d30d31e8879eac2d23fd6c23a8b694a519
Geoff <geoff@geofflinsley.com>
2019-03-21 17:03:56 +0000

    Update data_averager_test.py

--- data_averager_test.py
+++ data_averager_test.py
@@ -15,9 +15,19 @@ def test_dataGrabberFail():
 
 def test_dataGrabberPass():
     
-    pass_test = '/home/apesch/geoff_sample_data.txt'
+    working_sample = [
+        ['Year Mon Day Hr DOY from start Fractional DOY '
+         'Fractional year Dst HER KAK HON SJG sigma'],
+        ['1958 1 1 1 0.10208333333E+01 0.10208333333E+01 0.19580000571E+04 '
+         '-113.6 -126.4 -86.1 -89.0 -152.8 31.9'], 
+        ['1958 1 1 2 0.10625000000E+01 0.10625000000E+01 0.19580001712E+04 '
+         '-104.3 -117.3 -78.4 -90.4 -130.9 24.1']
+    ]
     
-    with patch.object(sys, 'argv', pass_test):
-        averager_result = data_averager.dataGrabber()
-        assert type(averager_result) == list
-        assert len(averager_result) > 1
+    averager_result = data_averager.dataGrabber()
+    assert type(averager_result) == list
+    assert len(averager_result) >= 1
+    assert averager_result[0] == [
+        'Year Mon Day Hr DOY from start Fractional DOY Fractional year Dst HER '
+        'KAK HON SJG sigma'
+        ]
